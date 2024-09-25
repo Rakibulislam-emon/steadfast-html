78284b234cfe1928804536b53ee8d9aea0ffa882
Rakibulislam-emon <rakibulislamemon60>
2024-09-25 12:05:14 +0600

    final commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    const sidebar = document.getElementById('sidebar');
+    const isVisible = sidebar.style.display === 'block';
+    
+    // Toggle sidebar visibility
+    sidebar.style.display = isVisible ? 'none' : 'block';
+}
